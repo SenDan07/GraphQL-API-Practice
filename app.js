c28e9a3ab26886048aee9c8c897e1226ee44aa3d
SenDan07 <dany.alekrmz@gmail.com>
2023-06-21 04:04:51 -0700

    Added post fetching, pagination and image upload to the server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 
@@ -59,6 +60,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if(!req.isAuth){
+    throw new Error('Not authenticated.')
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored successfully', filePath: req.file.path });
+});
+
 //Unlike REST APIs which makes use of several routes, in GraphQL this is the only endpoint we'll provide
 app.use(
   '/graphql',
@@ -100,3 +116,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
